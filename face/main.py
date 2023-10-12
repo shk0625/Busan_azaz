e184e11a70196eb9b79101ca613a5cee67f134c6
shk0625 <tjfgml1238@gmail.com>
2023-10-12 14:44:09 +0900

    Update :: main.py

--- face/main.py
+++ face/main.py
@@ -1,9 +1,15 @@
+import cv2
 import mediapipe as mp
 
-from face.Visualization import detector
-from face.views import visualize
+from Visualization import detector
+from views import visualize
+
+IMAGE_FILE = 'image.jpg'
 
 image = mp.Image.create_from_file(IMAGE_FILE)
 detection_result = detector.detect(image)
 
 visualized_image, num_faces_detected = visualize(image, detection_result)
+
+img = cv2.imread(IMAGE_FILE)
+cv2.imshow(img)
